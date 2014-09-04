e25c02eb570f0e82a5d8963440578a4dd1b636e9
Arturas Lebedevas <metarouter@gmail.com>
2014-09-04 17:57:25 +0100

    [basic] appView will display content constrained by size

--- examples/basic/views/AppView.js
+++ examples/basic/views/AppView.js
@@ -1,7 +1,14 @@
-var View = require('famous/core/View');
+var View       = require('famous/core/View'),
+    Modifier   = require('famous/core/Modifier'),
+    RenderNode = require('famous/core/RenderNode');
 
 function AppView () {
   View.apply(this, arguments);
+
+  this._rootModifier = new Modifier({
+    size: [undefined, 250]
+  });
+  this._rootNode = this.add(new RenderNode(this._rootModifier));
 }
 
 AppView.prototype = Object.create(View.prototype);
