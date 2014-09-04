92e769f4fe9af0f298a3dc14fdbf8631387fe1d3
Arturas Lebedevas <metarouter@gmail.com>
2014-09-04 15:13:16 +0100

    scripts task will browserify all app.js files in examples dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
-var gulp = require('gulp'),
-    path = require('path');
+var gulp       = require('gulp'),
+    gutil      = require('gulp-util'),
+    glob       = require('glob'),
+    path       = require('path'),
+    browserify = require('browserify'),
+    watchify   = require('watchify'),
+    async      = require('async'),
+    source     = require('vinyl-source-stream');
 
 var CURRENT_DIR = path.join.bind(path, process.cwd()),
     SRC_DIR     = CURRENT_DIR.bind(null, 'examples'),
@@ -12,6 +18,32 @@ gulp.task('static', function () {
   var destination = BUILD_DIR();
 
   gulp.src(staticFiles).pipe(gulp.dest(destination));
+});
+
+function buildApp (bw, appFileName, destination, done) {
+  bw.bundle()
+    .pipe(source(appFileName))
+    .pipe(gulp.dest(destination))
+    .on('end', done);
+}
+
+gulp.task('scripts', function (done) {
+  var appFileName = 'app.js';
+  glob(SRC_DIR('**', appFileName), function (err, files) {
+    if (err) {
+      return gutil.log(err);
+    }
+
+    async.each(files, function (file, done) {
+      var b = browserify(file, watchify.args),
+          w = watchify(b);
+      var destination = BUILD_DIR(file.split(path.sep).reverse()[1]),
+          build = buildApp.bind(null, w, appFileName, destination);
+
+      build(done);
+      w.on('update', build.bind(null, Function.prototype));
+    }, done);
+  });
 });
 
 gulp.task('default', function () {
