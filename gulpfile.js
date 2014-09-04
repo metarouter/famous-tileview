fe184883d0753a6b1e03d0a763e063a0c3ffaa1c
Arturas Lebedevas <metarouter@gmail.com>
2014-09-04 13:47:05 +0100

    copy html, css files to build directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    path = require('path');
+
+var CURRENT_DIR = path.join.bind(path, process.cwd()),
+    SRC_DIR     = CURRENT_DIR.bind(null, 'examples'),
+    BUILD_DIR   = CURRENT_DIR.bind(null, 'build');
+
+gulp.task('static', function () {
+  var staticFiles = ['*.html', '*.css'].map(SRC_DIR.bind(null, '**'));
+  var destination = BUILD_DIR();
+
+  gulp.src(staticFile, { read: false }).pipe(gulp.dest(destination));
+});
 
 gulp.task('default', function () {
 
