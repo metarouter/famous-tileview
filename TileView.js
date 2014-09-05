90ab17c6cab56e40706168c76419432b01afc604
Arturas Lebedevas <metarouter@gmail.com>
2014-09-05 13:29:19 +0100

    TileView #sequenceFrom method

--- TileView.js
+++ TileView.js
@@ -1,13 +1,19 @@
-var View     = require('famous/core/View'),
-    Scroller = require('famous/views/Scroller'),
-    Utility  = require('famous/utilities/Utility');
+var View       = require('famous/core/View'),
+    Modifier   = require('famous/core/Modifier'),
+    RenderNode = require('famous/core/RenderNode'),
+    Scroller   = require('famous/views/Scroller'),
+    Utility    = require('famous/utilities/Utility');
 
 function TileView () {
   View.apply(this, arguments);
 
+  this._items = [];
+
   this._scroller = new Scroller({
     direction: Utility.Direction.X
   });
+
+  this.add(this._scroller);
 }
 
 TileView.prototype = Object.create(View.prototype);
@@ -16,3 +22,8 @@ TileView.prototype.constructor = TileView;
 TileView.DEFAULT_OPTIONS = {};
 
 module.exports = TileView;
+
+TileView.prototype.sequenceFrom = function (node) {
+  this._scroller.sequenceFrom(node);
+  this._items = this._scroller._node._.array;
+};
